9e28cf0316bf26a335cf94bed955b052df43f8a5
Wolfgang <w.winter@logitags.com>
2014-12-13 16:14:51 +0100

    code formatting changed

--- src/main/java/de/danielbechler/diff/introspection/StandardIntrospector.java
+++ src/main/java/de/danielbechler/diff/introspection/StandardIntrospector.java
@@ -32,59 +32,69 @@ import de.danielbechler.util.Assert;
 import de.danielbechler.util.Exceptions;
 
 /**
- * Resolves the accessors of a given type by using the standard Java
- * {@link Introspector}.
- * 
+ * Resolves the accessors of a given type by using the standard Java {@link Introspector}.
+ *
  * @author Daniel Bechler
  */
-public class StandardIntrospector implements de.danielbechler.diff.introspection.Introspector {
+public class StandardIntrospector implements de.danielbechler.diff.introspection.Introspector
+{
 
    private static final Logger log = LoggerFactory.getLogger(StandardIntrospector.class);
 
-   public TypeInfo introspect(final Class<?> type) {
-      Assert.notNull(type, "type");
-      try {
-         return internalIntrospect(type);
-      } catch (final IntrospectionException e) {
-         throw Exceptions.escalate(e);
-      }
-   }
+	public TypeInfo introspect(final Class<?> type)
+	{
+		Assert.notNull(type, "type");
+		try
+		{
+			return internalIntrospect(type);
+		}
+		catch (final IntrospectionException e)
+		{
+			throw Exceptions.escalate(e);
+		}
+	}
 
-   private TypeInfo internalIntrospect(final Class<?> type) throws IntrospectionException {
-      final TypeInfo typeInfo = new TypeInfo(type);
-      final PropertyDescriptor[] descriptors = getBeanInfo(type).getPropertyDescriptors();
-      for (final PropertyDescriptor descriptor : descriptors) {
-         if (shouldSkip(descriptor)) {
-            continue;
-         }
-         final String propertyName = descriptor.getName();
+	private TypeInfo internalIntrospect(final Class<?> type) throws IntrospectionException
+	{
+		final TypeInfo typeInfo = new TypeInfo(type);
+		final PropertyDescriptor[] descriptors = getBeanInfo(type).getPropertyDescriptors();
+		for (final PropertyDescriptor descriptor : descriptors)
+		{
+			if (shouldSkip(descriptor))
+			{
+				continue;
+			}
+			final String propertyName = descriptor.getName();
          Field field = findField(type, propertyName);
-         final Method readMethod = descriptor.getReadMethod();
-         final Method writeMethod = descriptor.getWriteMethod();
+			final Method readMethod = descriptor.getReadMethod();
+			final Method writeMethod = descriptor.getWriteMethod();
          final PropertyAwareAccessor accessor = new PropertyAccessor(propertyName, field, readMethod, writeMethod);
-         typeInfo.addPropertyAccessor(accessor);
-      }
-      return typeInfo;
-   }
+			typeInfo.addPropertyAccessor(accessor);
+		}
+		return typeInfo;
+	}
 
-   protected BeanInfo getBeanInfo(final Class<?> type) throws IntrospectionException {
-      return Introspector.getBeanInfo(type);
-   }
+	protected BeanInfo getBeanInfo(final Class<?> type) throws IntrospectionException
+	{
+		return Introspector.getBeanInfo(type);
+	}
 
-   private static boolean shouldSkip(final PropertyDescriptor descriptor) {
-      if (descriptor.getName().equals("class")) // Java & Groovy
-      {
-         return true;
-      }
-      if (descriptor.getName().equals("metaClass")) // Groovy
-      {
-         return true;
-      }
-      if (descriptor.getReadMethod() == null) {
-         return true;
-      }
-      return false;
-   }
+	private static boolean shouldSkip(final PropertyDescriptor descriptor)
+	{
+		if (descriptor.getName().equals("class")) // Java & Groovy
+		{
+			return true;
+		}
+		if (descriptor.getName().equals("metaClass")) // Groovy
+		{
+			return true;
+		}
+		if (descriptor.getReadMethod() == null)
+		{
+			return true;
+		}
+		return false;
+	}
 
    private Field findField(final Class<?> type, final String propertyName) throws IntrospectionException {
       String fieldName = propertyName.substring(0, 1).toLowerCase() + propertyName.substring(1);
