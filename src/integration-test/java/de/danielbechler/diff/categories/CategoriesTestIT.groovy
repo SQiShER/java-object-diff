a020094638bd943d0df2831b8bae746b76572d47
Mauro Scomparin <scompo@gmail.com>
2016-03-07 19:21:35 +0100

    Added categories from configuration to DiffNode on comparison.
    Added DiffNode.addCategories(Collection<String> categories)
    so users can add their own categories while traversing nodes.
    Made DiffNode.getCategories return an UnmodifiableSet to discourage
    users from editing the returned Set.
    Implemented unit tests for categories manipulation in DiffNodeTest.
    Added CategoriesTestIT integration test for categories resolution.

---
+++ src/integration-test/java/de/danielbechler/diff/categories/CategoriesTestIT.groovy
@@ -0,0 +1,45 @@
+package de.danielbechler.diff.categories
+
+import de.danielbechler.diff.ObjectDifferBuilder
+import de.danielbechler.diff.introspection.ObjectDiffProperty
+import de.danielbechler.diff.node.DiffNode
+import de.danielbechler.diff.node.Visit
+import de.danielbechler.diff.path.NodePath
+import spock.lang.Specification
+
+class CategoriesTestIT extends Specification{
+
+	def "should return all categories"(){
+		setup:
+			def obj1 = new MyObject("aaa","aaa")
+			def obj2 =  new MyObject("bbb","bbb")
+			def differ = ObjectDifferBuilder.startBuilding()
+					.categories()
+					.ofNode(NodePath.with("firstString")).toBe("cat1")
+					.ofNode(NodePath.with("secondString")).toBe("cat1")
+					.and()
+					.build()
+			def node = differ.compare(obj1,obj2)
+
+		expect :
+			node.getChild("firstString").getCategories() == ["cat1"] as Set
+			node.getChild("secondString").getCategories() == ["cat1", "catAnnotation"] as Set
+	}
+
+	class MyObject{
+
+		def firstString
+        def secondString
+
+		MyObject(firstString,secondString) {
+
+			this.firstString = firstString
+			this.secondString = secondString
+		}
+
+        @ObjectDiffProperty(categories = ["catAnnotation"])
+		def getSecondString() {
+			return secondString
+		}
+	}
+}
