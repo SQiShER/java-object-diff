8ef3a14199b61d9088ad5a5bdd665a243835ce15
Daniel Bechler <SQiShER@users.noreply.github.com>
2016-03-12 01:53:41 +0100

    Added test for Visit class

---
+++ src/test/java/de/danielbechler/diff/node/VisitTest.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 Daniel Bechler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.danielbechler.diff.node
+
+import spock.lang.Specification
+
+class VisitTest extends Specification {
+
+	def "Stop"() {
+		given:
+		  final visit = new Visit()
+		expect:
+		  !visit.stopped
+		when:
+		  visit.stop()
+		then:
+		  visit.stopped
+	}
+
+	def "DontGoDeeper"() {
+		given:
+		  final visit = new Visit()
+		expect:
+		  visit.allowedToGoDeeper
+		when:
+		  visit.dontGoDeeper()
+		then:
+		  !visit.allowedToGoDeeper
+	}
+}
